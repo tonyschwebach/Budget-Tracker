7e7a56d4922fc417e6ac66d39cac9b33de399c0d
T Schway <tony.schwebach@gmail.com>
2021-02-09 10:32:49 -0500

    added mongodb environment variable

--- server.js
+++ server.js
@@ -15,9 +15,23 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://localhost/budget", {
+
+
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/budget", {
   useNewUrlParser: true,
-  useFindAndModify: false
+  useUnifiedTopology: true,
+  useFindAndModify: false,
+  useCreateIndex: true,
+});
+
+const connection = mongoose.connection;
+
+connection.on("connected", () => {
+  console.log("Mongoose successfully connected.");
+});
+
+connection.on("error", (err) => {
+  console.log("Mongoose connection error: " + err);
 });
 
 // routes
